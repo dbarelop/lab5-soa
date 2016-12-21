3f97bf6067983ef909c1b20e29d7e083d5af07ab
aitorsanz <aitor_sanz@hotmail.com>
2016-12-21 22:52:44 +0100

    Resolving conflicts

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,16 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-
 
 @Controller
 public class SearchController {
 
-	@Autowired
-	  private ProducerTemplate producerTemplate;
+    @Autowired
+    private ProducerTemplate producerTemplate;
 
-	@RequestMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
@@ -25,16 +23,6 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        HashMap<String, Object> header = new HashMap<>();
-        int index=q.indexOf("max:");
-        if(index!=-1){
-            int numIndex = index + 4;
-            String num = q.substring(numIndex);
-            int number = Integer.parseInt(num);
-            q = q.substring(0, index-1);
-            header.put("CamelTwitterCount", number);
-        }
-        header.put("CamelTwitterKeywords", q);
-        return producerTemplate.requestBodyAndHeaders("direct:search", "", header);
+        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
     }
 }
